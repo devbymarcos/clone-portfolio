5bc244d2649f7c9898aaf48f1bcae88604764b91
Marcos <marcoslopes.dev@gmail.com>
2023-06-01 10:47:06 -0300

    refactor grid

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import "@splidejs/splide/css";
 import menuMobileAction from "./module/menu-mobile";
 import typingEffect from "./module/typingEffect";
 import slidejs from "./module/slide";
+import filterPortfolio from "./module/filterPortfolio";
 menuMobileAction();
 typingEffect();
 slidejs();
+filterPortfolio();
