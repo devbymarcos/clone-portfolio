437380f70f93933202baaaca9e0c14d5b43fdf98
Marcos <marcoslopes.dev@gmail.com>
2023-06-01 22:35:31 -0300

    ajuste de tempo da animção filtro

--- module/filterPortfolio.js
+++ module/filterPortfolio.js
@@ -1,6 +1,7 @@
 export default function filterPortfolio() {
   const navPortfolio = document.querySelectorAll(".nav-portfolio li");
   const gridItem = document.querySelectorAll(".port-grid-item");
+  const gridContent = document.querySelector(".portfolio-grid");
 
   function filterPortfolio(e) {
     gridItem.forEach((elem) => {
@@ -9,14 +10,16 @@ export default function filterPortfolio() {
           elem.style.opacity = "0";
           setTimeout(() => {
             elem.style.display = "none";
-          }, 300);
+          }, 100);
         } else {
-          elem.style.display = "block";
           elem.style.opacity = "1";
+          setTimeout(() => {
+            elem.style.display = "block";
+          }, 200);
         }
       } else {
-        elem.style.display = "block";
         elem.style.opacity = "1";
+        elem.style.display = "block";
       }
     });
   }
