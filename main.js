6b6c81751eaf932e596296eb588d24167e7e2449
Marcos <marcoslopes.dev@gmail.com>
2023-06-01 23:30:25 -0300

    finalizado script aula portfolio

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import "@splidejs/splide/css";
 import menuMobileAction from "./module/menu-mobile";
 import typingEffect from "./module/typingEffect";
 import slidejs from "./module/slide";
+import filterPortfolio from "./module/filter-portfolio";
 menuMobileAction();
 typingEffect();
 slidejs();
+filterPortfolio();
---
+++ module/filter-portfolio.js
@@ -0,0 +1,29 @@
+export default function filterPortfolio() {
+  const navPortfolio = document.querySelectorAll(".nav-portfolio li");
+  const gridItem = document.querySelectorAll(".port-grid-item");
+
+  function runFilter(e) {
+    gridItem.forEach((elem) => {
+      if (e.target.dataset.filter != "all") {
+        if (elem.dataset.item != e.target.dataset.filter) {
+          elem.style.opacity = "0";
+          setTimeout(() => {
+            elem.style.display = "none";
+          }, 100);
+        } else {
+          elem.style.opacity = "1";
+          setTimeout(() => {
+            elem.style.display = "block";
+          }, 200);
+        }
+      } else {
+        elem.style.opacity = "1";
+        elem.style.display = "block";
+      }
+    });
+  }
+
+  navPortfolio.forEach((item) => {
+    item.addEventListener("click", runFilter);
+  });
+}
